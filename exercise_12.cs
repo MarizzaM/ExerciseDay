20ce82d02bf6f239c38e028869e32b9e013a69f1
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-12 18:06:33 +0200

    Create exercise_12.cs

---
+++ exercise_12.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ExerciseDay
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>() { 0, 1, 1, 2, 3, 5, 8, 13 };
+
+            Thread t = new Thread(() => {
+                Console.WriteLine("Hello world");
+            });
+            t.Start();
+
+            Thread t1 = new Thread(() => {
+                Console.WriteLine(list.Find(x => x==13));
+            });
+
+            t1.Start();
+        }
+    }
+}
