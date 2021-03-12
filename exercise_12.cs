02eb59f20d24edcada576416269f2501142574e8
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-12 18:07:07 +0200

    Update exercise_12.cs

--- exercise_12.cs
+++ exercise_12.cs
@@ -16,12 +16,12 @@ namespace ExerciseDay
         {
 
             List<int> list = new List<int>() { 0, 1, 1, 2, 3, 5, 8, 13 };
-
+//a
             Thread t = new Thread(() => {
                 Console.WriteLine("Hello world");
             });
             t.Start();
-
+//b
             Thread t1 = new Thread(() => {
                 Console.WriteLine(list.Find(x => x==13));
             });
